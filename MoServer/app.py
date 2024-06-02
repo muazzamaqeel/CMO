2b86ab0a78fb9cbd76070828228825f857df7466
muazzamaqeel <aqeemu01@thu.de>
2024-06-02 16:43:17 +0200

    WEBSITE

--- MoServer/app.py
+++ MoServer/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets', template_folder='.')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
-@app.route('/<path:path>')
-def serve_file(path):
-    return send_from_directory('.', path)
+@app.route('/<path:filename>')
+def serve_static_files(filename):
+    if filename.startswith('assets/'):
+        return send_from_directory('assets', filename[len('assets/'):])
+    elif filename.startswith('forms/'):
+        return send_from_directory('forms', filename[len('forms/'):])
+    else:
+        return render_template(filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8000)
